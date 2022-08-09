1b5c8b326d66749d70d9cdbc5c34d5282b65a3e4
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-08-09 22:43:50 +0530

    create file upload function and restricted to only upload image files

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export default function uploadForm(){
+export default function UploadForm(){
+    
+    const [file, setFile] = useState(null);
+
+    const allowedTypes = ['image/png', 'image/jpeg'];
+
+    const changeHandler = (e) => {
+        let selected = e.target.files[0];
+        console.log(selected);
+
+        if (selected && allowedTypes.includes(selected.type)) {
+            setFile(selected);
+        }
+    }
+
     return(
         <form>
-            <input type="file" />
+            <input type="file" onChange={changeHandler}/>
         </form>
     );
 }
